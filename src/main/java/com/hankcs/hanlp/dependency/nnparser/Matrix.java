e6403c8572e89c40527af74803d877b1cef6efaa
DoiMasayuki <m-doi@ist.osaka-u.ac.jp>
2019-12-25 21:08:25 +0900

    rename variables

--- src/main/java/com/hankcs/hanlp/dependency/nnparser/Matrix.java
+++ src/main/java/com/hankcs/hanlp/dependency/nnparser/Matrix.java
@@ -994,16 +994,16 @@ public class Matrix implements Cloneable, java.io.Serializable, ICacheAble
 
     public static Matrix random(int m, int n)
     {
-        Matrix A = new Matrix(m, n);
-        double[][] X = A.getArray();
+        Matrix X = new Matrix(m, n);
+        double[][] C = X.getArray();
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                X[i][j] = Math.random();
+                C[i][j] = Math.random();
             }
         }
-        return A;
+        return X;
     }
 
     /**
